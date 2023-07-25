62d94f3a8dcaee4bca60764712abffc9730388e9
GiovanaJensen <oliveirasgiovana143@gmail.com>
2023-07-24 21:25:21 -0300

    criando frames

---
+++ MyFrame.java
@@ -0,0 +1,18 @@
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame{
+    MyFrame(){
+        this.setTitle("My first project"); //sets title of frame
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit out of application
+        this.setResizable(false); // prevent the frame from being resized
+        this.setSize(420,420); // sets the x-dimension and y-dimension of frame
+        this.setVisible(true); // make frame visible
+
+        ImageIcon image = new ImageIcon("C:\\workspace\\java\\java_swing\\src\\logo.png"); //create an ImageIcon
+        this.setIconImage(image.getImage()); //change icon of the frame
+
+        this.getContentPane().setBackground(new Color(0x123456)); //change the background
+    }
+}

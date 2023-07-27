19d18456245a5c347c5ae8ac82df016610ccd6a5
GiovanaJensen <oliveirasgiovana143@gmail.com>
2023-07-26 21:44:16 -0300

    comecando a utilizar panels

--- App.java
+++ App.java
@@ -1,7 +1,39 @@
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.Color;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        new MyFrame();
+        // new MyFrame();
 
-        new LabelFrame();
+        // new LabelFrame();
+
+        JLabel label = new JLabel();
+        ImageIcon icon = new ImageIcon("src\\assets\\logo.png");
+        label.setIcon(icon);
+
+        JPanel redPanel = new JPanel();
+        redPanel.setBackground(Color.red);
+        redPanel.setBounds(0, 0, 250, 250);
+
+        JPanel bluePanel = new JPanel();
+        bluePanel.setBackground(Color.blue);
+        bluePanel.setBounds(250, 0, 250, 250);
+
+        JPanel greenPanel = new JPanel();
+        greenPanel.setBackground(Color.green);
+        greenPanel.setBounds(0, 250, 500, 250);
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setSize(720,720);
+        frame.setVisible(true);
+        greenPanel.add(label);
+        frame.add(redPanel);
+        frame.add(greenPanel);
+        frame.add(bluePanel);
     }
 }

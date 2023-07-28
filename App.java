5c3f4ab7506ef6b880862a6aaa137edeb41194ae
GiovanaJensen <oliveirasgiovana143@gmail.com>
2023-07-28 20:24:17 -0300

    organizando arquivo sorbe panel

--- App.java
+++ App.java
@@ -1,44 +1,9 @@
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import java.awt.Color;
-import java.awt.BorderLayout;
-
 public class App {
     public static void main(String[] args) throws Exception {
         // new MyFrame();
 
         // new LabelFrame();
 
-        JLabel label = new JLabel();
-        ImageIcon icon = new ImageIcon("src\\assets\\logo.png");
-        label.setIcon(icon);
-        // label.setVerticalAlignment(JLabel.TOP);
-        label.setBounds(0,0,75,75);
-
-        JPanel redPanel = new JPanel();
-        redPanel.setBackground(Color.red);
-        redPanel.setBounds(0, 0, 250, 250);
-
-        JPanel bluePanel = new JPanel();
-        bluePanel.setBackground(Color.blue);
-        bluePanel.setBounds(250, 0, 250, 250);
-
-        JPanel greenPanel = new JPanel();
-        greenPanel.setBackground(Color.green);
-        greenPanel.setBounds(0, 250, 500, 250);
-        // greenPanel.setLayout(new BorderLayout());
-        greenPanel.setLayout(null);
-
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(null);
-        frame.setSize(720,720);
-        frame.setVisible(true);
-        greenPanel.add(label);
-        frame.add(redPanel);
-        frame.add(greenPanel);
-        frame.add(bluePanel);
+        new Panel();
     }
 }
